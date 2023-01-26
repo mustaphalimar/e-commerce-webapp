41dd14a61ab53aa4f32b47293f2890362c18c381
Mustapha Limar <58789886+limaroov@users.noreply.github.com>
2023-01-26 01:48:31 +0100

    ignored dist folder

--- server/dist/trpc/routers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appRouter = void 0;
-const trpc_1 = require("../trpc");
-const product_1 = require("./product");
-exports.appRouter = (0, trpc_1.mergeRouters)(product_1.productRouter);
